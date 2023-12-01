986ee54a7814244045aaa6543b800df39112275e
yu2615 <yu.2615.sk@gmail.com>
2023-12-01 16:43:24 +0900

    文章の変更

--- image-g/src/components/Header.js
+++ image-g/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
           <h1 className="app-title">ランダム壁紙アプリ</h1>
           <p className="app-description">
             このアプリでは、美しいランダムな壁紙を生成してダウンロードできます。<br />
-            保存した画像は一覧で簡単に閲覧できます。
+            新しい画像に出会いましょう！
           </p>
         </div>
       </div>
